1741e0dfb50b3d9d2908239f6c5a2aaaa67c6dfe
ammaralazki <alazkiyasser3@gmail.com>
2026-07-05 09:37:41 +0400

    Task 9

--- task/Program.cs
+++ task/Program.cs
@@ -170,8 +170,44 @@
             Console.WriteLine($"Discount amount: {discount:F3} OMR");
             Console.WriteLine($"Final amount to pay: {finalAmount:F3} OMR");
             
-                
+             //Task-9-------------------------------------------------------------
+             //Day Name Finder----------------------------------------------------
+        
+            Console.Write("Enter a number (1-7) to find the day of the week: ");
+            int dayNumber = Convert.ToInt32(Console.ReadLine());
 
+            string dayName;
+
+            switch (dayNumber)
+            {
+                case 1:
+                    dayName = "Sunday";
+                    break;
+                case 2:
+                    dayName = "Monday";
+                    break;
+                case 3:
+                    dayName = "Tuesday";
+                    break;
+                case 4:
+                    dayName = "Wednesday";
+                    break;
+                case 5:
+                    dayName = "Thursday";
+                    break;
+                case 6:
+                    dayName = "Friday";
+                    break;
+                case 7:
+                    dayName = "Saturday";
+                    break;
+                default:
+                    dayName = "Invalid day number";
+                    break;
+            }
+
+            Console.WriteLine(dayName);
+            
         }
     }
 }
