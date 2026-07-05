d314ffa6c47a0708ceb941cf9a5c402c67be21e5
ammaralazki <alazkiyasser3@gmail.com>
2026-07-05 09:49:20 +0400

    Task 10

--- task/Program.cs
+++ task/Program.cs
@@ -208,6 +208,63 @@
 
             Console.WriteLine(dayName);
             
+            //Task-10--------------------------------------------------------
+            //Mini Calculator------------------------------------------------
+            
+                    // Ask the user for two numbers and an operator
+            Console.Write("Enter first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter an operator (+, -, *, /, %): ");
+            char op = Convert.ToChar(Console.ReadLine());
+
+            double result = 0;
+            bool isValid = true;
+
+            switch (op)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 != 0)
+                        result = num1 / num2;
+                    else
+                    {
+                        Console.WriteLine("Cannot divide by zero");
+                        isValid = false;
+                    }
+                    break;
+                case '%':
+                    if (num2 != 0)
+                        result = num1 % num2;
+                    else
+                    {
+                        Console.WriteLine("Cannot divide by zero");
+                        isValid = false;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid operator");
+                    isValid = false;
+                    break;
+            }
+
+            if (isValid)
+            {
+                Console.WriteLine($"Result: {result}");
+            }
+
+            
         }
     }
 }
